04e9ce63e91d086bd4301ab49866def2fa3c22f8
dashabespalova <dasha_box1@mail.ru>
2016-02-08 12:06:40 +0200

    first

--- js/main.js
+++ js/main.js
@@ -1,3 +1,42 @@
 ;(function($){
+	$(function(){
+		var slider = $('.ba-slider');
+		slider.slick({
+			dots:true,
+			gorizontal:true,
+			infinity: false
+		});
+		var qtyField = $('.ba-product__qty-field'),
+		qtyUp = $('.ba-product__qty-up'),
+		qtyDown = $('.ba-product__qty-down');
+		qtyUp.on('click', function(event){
+			event.preventDefault();
+			var oldVal = qtyField.val();
+			qtyField.val(++oldVal);
+		});
+		qtyDown.on('click', function(event){
+			event.preventDefault();
+			var oldVal = qtyField.val();
+			if (--oldVal > 0){
+				qtyField.val(oldVal);
+			}
+		});
 
+		var prodColor = $('[name="product_color"]'),
+			prodImg = $('#product_img'),
+			prodTitle = $('.ba-product__title > span');
+		prodColor.on('change', function(){
+
+			if (this.value === "black"){
+				prodImg.attr({'src': 'img/hp-black.png', 'alt': 'Black headphones'});
+
+				prodTitle.html("Black");
+				// prodImg.fadeIn('slow');
+			} else {
+				prodImg.attr({'src': 'img/hp-brown.png', 'alt': 'Brown headphones'});
+
+				prodTitle.html("Brown");
+			}
+		});
+	});
 })(jQuery);
